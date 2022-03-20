f984e6a1c2b053e37633cd25ebc66f770ef8e8ea
NotARoomba1 <70660308+NotARoomba@users.noreply.github.com>
2022-03-20 16:46:05 +0000

    gitignore

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+/**
+ * https://vitejs.dev/config/
+ * @type { import('vite').UserConfig }
+ */
+export default {
+  plugins: [reactRefresh()],
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      port: 443,
+    }
+  },
+  build: {
+    outDir: './docs'
+  }
+
+}
