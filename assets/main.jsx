97bbc52d831f8c6518d8f91e921e8c48726e9ad2
NotARoomba1 <70660308+NotARoomba@users.noreply.github.com>
2022-03-21 02:31:48 +0000

    hashrouter for github pages

--- assets/main.jsx
+++ assets/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './css/App.css'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './js/App'
 
 ReactDOM.render(
-     <BrowserRouter>
+     <HashRouter>
     <App />
-    </BrowserRouter>,
+    </HashRouter>,
   document.getElementById('root')
 )
