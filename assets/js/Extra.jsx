8a46e5cbfe14a69b756f3d349f0f2e8162122f93
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-07-28 17:33:15 -0500

    refactor extra and added RoombaOS and Wordle

--- assets/js/Extra.jsx
+++ assets/js/Extra.jsx
@@ -13,11 +13,12 @@ export default function Extra() {
       </div>
       <div className="home-button-section">
         <Link className="link-button" to="/" >Home</Link>
-        <Link className="link-button" onClick={() => window.location.href = "https://koombia.notaroomba.xyz"} >Koombia</Link>
-        <Link className="link-button" onClick={() => window.location.href = "https://grindhub.notaroomba.xyz"}>Grindhub</Link>
-        <Link className="link-button" onClick={() => window.location.href = "https://devoided.notaroomba.xyz"}>Devoided</Link>
-        <Link className="link-button" onClick={() => window.location.href = "https://gravity.notaroomba.xyz"}>Gravity</Link>
-        <Link className="link-button" onClick={() => window.location.href = "https://status.notaroomba.xyz"}>Status</Link>
+        <Link className="link-button" to="https://koombia.notaroomba.xyz">Koombia</Link>
+        <Link className="link-button" to="https://grindhub.notaroomba.xyz">Grindhub</Link>
+        <Link className="link-button" to="https://devoided.notaroomba.xyz">Devoided</Link>
+        <Link className="link-button" to="https://gravity.notaroomba.xyz">Gravity</Link>
+        <Link className="link-button" to="https://wordle.notaroomba.xyz">Wordle</Link>
+        <Link className="link-button" to="https://status.notaroomba.xyz">Status</Link>
       </div>
       <div id="secret-text">
           <p className="wrap">This page was made in memorial to the dozens of poor souls murdered by Nathan's confusing and mind boggling code. He frequently create codes that makes literally no sense and give to his teacher and fry his brain. The wonderful projects are posted here for you to die. <br/> -Henry</p>
