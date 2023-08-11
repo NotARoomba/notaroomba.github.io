f1b6945fbd5ed7df2a84d41824b9c4398b00bc4c
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-08-11 16:02:24 -0500

    about me page looking better

--- assets/js/About.jsx
+++ assets/js/About.jsx
@@ -18,18 +18,33 @@ export default function About() {
           </div>
         </div>
       </div>
-      <div className="text-box-outer">
-        <div className="text-box-inner">
+      <div className="outer-outer-box">
+        <div className="text-box-outer">
             <p>I am a high school student that likes to break things for fun. My main languages are JavaScript, C++, and Java.</p>
         </div>
-        <div className="text-box-text">
-          Frameworks
-        </div>
-        <div className="text-box-inner">
+        <div className="text-box-outer">
             <p>Check out my website for more information such as my projects, status of my websites, and API’s. There may be a secret somewhere...</p>
+      </div>
+      </div>
+      <div className="about-frameworks">
+        <p>Skills</p>
+        <div id="image-box" className="text-box-outer">
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" />
+        <img className="dark-icons"src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/unity/unity-original.svg" />
         </div>
       </div>
-      <div id="image-box" className="text-box-outer">
+      {/* <div id="image-box" className="text-box-outer">
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" />
         <img src="/assets/img/archlinux.svg"/>
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firefox/firefox-original.svg" /> 
@@ -54,7 +69,7 @@ export default function About() {
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg" />  
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" />
-      </div>
+      </div> */}
       <Circles/>
     </Transitions>)
 }
